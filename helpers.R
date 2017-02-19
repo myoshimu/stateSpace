b566e84982644ec32e0c828c563f6eb5552ab0c4
Miki <myoshimu@users.noreply.github.com>
2017-02-19 21:13:36 +0900

    Update helpers.R

--- helpers.R
+++ helpers.R
@@ -1,19 +1,4 @@
-# R programs for 
-#    Commandeur, J.J.F. & Koopman, S.J. (2007)
-#    "An Introduction to State Space Time Series Analysis,"
-#    Oxford University Press.
-#
-# notes:
-#    prefixes: 
-#      n  : iNteger
-#      g  : numeric (g in "sinGle")
-#      b  : binary  
-#      a  : vector  (not sure where it comes from)
-#      m  : matrix
-#      l  : list
-#      ts : ts object (Time Series)
-#      o  : object of any class
-#
+
 # # # # # # # # # # # # # # # # # # #
 #       some helper routines        #
 # # # # # # # # # # # # # # # # # # #
