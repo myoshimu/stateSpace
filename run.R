b4ed5fd20724394bf4854aadd4b95c9695eb804b
Miki <myoshimu@users.noreply.github.com>
2017-02-19 21:27:12 +0900

    Create run.R

---
+++ run.R
@@ -0,0 +1,43 @@
+
+# png(width = 480, height = 300)
+
+# exercise.Chapter1 ()
+#  
+# exercise.Chapter2.1 ()
+# exercise.Chapter2.2 ()
+# exercise.Chapter2.3 () 
+# 
+# exercise.Chapter3.1 ()
+# exercise.Chapter3.2 ()
+# exercise.Chapter3.3 ()
+# exercise.Chapter3.4a ()
+# exercise.Chapter3.4b ()  
+# 
+# exercise.Chapter4.1 ()
+exercise.Chapter4.2 ()
+# exercise.Chapter4.3 () 
+# exercise.Chapter4.4 ()
+#  
+# exercise.Chapter5.1a ()
+# exercise.Chapter5.1b ()
+# exercise.Chapter5.2 ()
+#  
+# exercise.Chapter6.1 ()
+# exercise.Chapter6.2 ()
+# 
+# exercise.Chapter7.1 ()
+# exercise.Chapter7.2 ()
+# exercise.Chapter7.3 ()
+# exercise.Chapter7.4 ()
+# 
+# exercise.Chapter8.6a ()
+# exercise.Chapter8.6b () 
+# exercise.Chapter8.6c () 
+# exercise.Chapter8.7 () 
+# 
+# exercise.Chapter9.4a () 
+# exercise.Chapter9.4b () 
+# 
+# exercise.Chapter10 () 
+
+# dev.off()
