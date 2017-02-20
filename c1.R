1b40ae5576bd2809691c523a52dbbb205fcb2a39
Miki <myoshimu@users.noreply.github.com>
2017-02-20 09:06:33 +0900

    Update c1.R

--- c1.R
+++ c1.R
@@ -1,51 +1,32 @@
-# # # # # # # # # # # # # # # # # # #
-#          main routines            #
-# # # # # # # # # # # # # # # # # # #
-# - - - - - - - - - - - - -- -
-exercise.Chapter1 <- function(){
+df <- read.table("data/UKdriversKSI.txt", skip=1)
+d <- ts(log(df[,1]))
+
+# linear regression
+tslm <- lm(d ~ time(d))
+print(tslm)
+
+plot(d, type = "p",pch  = 3, xlab = NA, ylab = NA, main = "Figure 1.1")
+abline(tslm, col="red")
+#legend("top", legend = c("log UK drivers KSI against time (in months)", "regression line"),  lty = c(-1, 1),    pch = c(3, -1), 
+#col = c("black", "red"),cex = .8)
+
+# time series
+plot(d, type = "n", xlab = NA, ylab = NA,  main = "Figure 1.2")
+lines(d, col = "black")
+legend("top", legend = "log UK drivers KSI", lty= 1, col= c("black", "red"),cex= .8)
+
+
+plot(d - predict(tslm),type="l",lty= 2,col= "green",xlab= NA, ylab  = NA, main  = "Figure 1.3")
+abline(h=0, col="gray")
+legend("top", lty= 2, legend = "residuals",  col= "green",cex = .8  )
   
-  # constants - - - - - -
-  sFILENAME <- "UKdriversKSI.txt"
-  # - - - - - - - - - - - 
-  
-  # reading data
-  dfData <- read.table(sFILENAME, skip=1)
-  tsData <- ts(log(dfData[,1]))
-  
-  # linear regression
-  cat("Linear regression: \n")
-  oOut.lm <- lm(tsData ~ time(tsData))
-  print(oOut.lm)
-  
-  # plot
-  sub.plotFigure_scatterreg(
-    tsData, 
-    tsData ~ time(tsData),
-    "Figure 1.1", 
-    c("log UK drivers KSI against time (in months)", "regression line")
-  )
-  sub.plotFigure_twoseries(
-    tsData, 
-    NULL, 
-    "Figure 1.2", 
-    "log UK drivers KSI"
-  )
-  sub.plotFigure_residual(
-    tsData - predict(oOut.lm), 
-    "Figure 1.3",
-    "residuals"
-  )
-  sub.plotFigure_ACF(
-    as.ts(rnorm(length(tsData))), 
-    15, 
-    "Figure 1.4", 
-    "ACF-random residuals"
-  )
-  sub.plotFigure_ACF(
-    tsData - predict(oOut.lm), 
-    15, 
-    "Figure 1.5", 
-    "ACF-regression residuals"
-  )
-  
-}
+
+#ACF
+acf(ts(as.ts(rnorm(length(d))), frequency=1), lag.max = 15,lwd = 12, col = "green"
+    , main = "Figure 1.4", xlab = NA, ylab = NA , xlim = c(1,15))
+legend("top", legend = "ACF-random residuals", lty= 1, col = "green", cex = .8) 
+
+
+acf(ts(d - predict(tslm), frequency=1), lag.max = 15,lwd = 12, col = "green"
+    , main = "Figure 1.5", xlab = NA, ylab = NA , xlim = c(1,15))
+legend("top", legend = "ACF-regression residuals", lty= 1, col = "green", cex = .8) 
